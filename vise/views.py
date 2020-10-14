10921892ac3b63d75d2cafb22261af47422b8c6c
Roman <roman.oneshchuk@gmail.com>
2020-10-14 19:49:20 +0300

    Reverse words + count words

--- vise/views.py
+++ vise/views.py
@@ -7,5 +7,5 @@ def home(request):
 def reverse(request):
 	user_text = request.GET['usertext']
 	reversed_text = user_text[::-1]
-	new_text = reversed_text[1::] * 5
-	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text, 'newtext':new_text})
+	count_words = len(reversed_text.split())
+	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text, 'countwords':count_words})
